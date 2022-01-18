3994a06b1611c5400e4ef75885ba5d9f6e99e7c0
moonyy7012 <moonyy7012@naver.com>
2022-01-18 21:28:49 +0900

    [Design] 레이아웃 초안 🎨

--- RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/MainActivity.kt
+++ RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.firechicken.rollingpictures_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val entranceButton = findViewById<ImageButton>(R.id.entranceButton)
+        entranceButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, GameWaitingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
