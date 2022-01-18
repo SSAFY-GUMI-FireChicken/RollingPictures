309073591f851dd3b3f57aea18a09c50bb0cb8d3
SSAFY <likppi100@naver.com>
2022-01-18 17:55:48 +0900

    [Design] 레이아웃 초안 🎨

--- RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/GameFinishActivity.kt
+++ RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/GameFinishActivity.kt
@@ -3,10 +3,9 @@ package com.firechicken.rollingpictures_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GameFinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_game_finish)
     }
 }
--- RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/LoginActivity.kt
+++ RollingPictures-Android/app/src/main/java/com/firechicken/rollingpictures_android/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.firechicken.rollingpictures_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener{
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
