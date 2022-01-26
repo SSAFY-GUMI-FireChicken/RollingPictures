8a8ba4e9ce9b96e8085dd0805f137e71d9567e77
Seungho Lee <seung7642@gmail.com>
2022-01-26 22:11:43 +0900

    [Feat] 전체 섹션 출력 API 개발

---
+++ RollingPictures-Backend/api-module/src/main/java/com/ssafy/api/controller/SectionController.java
@@ -0,0 +1,31 @@
+package com.ssafy.api.controller;
+
+import com.ssafy.api.dto.res.SectionResDTO;
+import com.ssafy.api.service.SectionService;
+import com.ssafy.api.service.common.ListResult;
+import com.ssafy.api.service.common.ResponseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Api(tags = {"04. 섹션"})
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class SectionController {
+    private final ResponseService responseService;
+    private final SectionService sectionService;
+
+    @ApiOperation(value = "섹션 조회", notes = "특정 시작 유저에 대한 섹션 조회")
+    @PostMapping("/section")
+    public ListResult<SectionResDTO> section(Long gameChannelId, Long userId) throws Exception {
+        List<SectionResDTO> section = sectionService.getSection(gameChannelId, userId);
+        return responseService.getListResult(section);
+    }
+}
--- RollingPictures-Backend/api-module/src/main/java/com/ssafy/api/repository/SectionRepository.java
+++ RollingPictures-Backend/api-module/src/main/java/com/ssafy/api/repository/SectionRepository.java
@@ -1,12 +1,14 @@
 package com.ssafy.api.repository;
 
 import com.ssafy.api.domain.Section;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Long> {
-
+    @EntityGraph(attributePaths = {"round"})
+    Optional<Section> findByStartOrderAndGameChannelId(Integer startOrder, Long gameChannelId);
 }
