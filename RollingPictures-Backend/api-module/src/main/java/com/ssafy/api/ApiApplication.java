7bfc32acbef8b596fe1219ab177387cad9414fe9
양아름 <smy04299@naver.com>
2022-01-28 11:13:03 +0900

    [Feat] 소켓 입퇴장 알림 처리 📰

--- RollingPictures-Backend/api-module/src/main/java/com/ssafy/api/ApiApplication.java
+++ RollingPictures-Backend/api-module/src/main/java/com/ssafy/api/ApiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
+@ComponentScan({"com.ssafy.socket"})
 @ComponentScan({"com.ssafy.core"})
 @ComponentScan({"com.ssafy.api"})
 @EntityScan({"com.ssafy.core", "com.ssafy.api"})
